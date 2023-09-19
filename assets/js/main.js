1804084799b5f14f8ef82bf0e8b337f7039ce1f4
nathaliaatamanchuk <nathaliaatamanchuk5@gmail.com>
2023-09-19 16:35:22 -0300

    promise and arrow function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 const offset = 0
 const limit = 10
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function (response) {
-    console.log(response)
-})
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
