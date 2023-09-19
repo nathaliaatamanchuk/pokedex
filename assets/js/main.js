78ba54f491d013681f23679c040084ba33b6cb81
nathaliaatamanchuk <nathaliaatamanchuk5@gmail.com>
2023-09-19 15:46:00 -0300

    first request

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const offset = 0
+const limit = 10
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+
+fetch(url).then(function (response) {
+    console.log(response)
+})
