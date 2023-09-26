a9ca5adf55b05be314fb7c405c94b482ee87985d
nathaliaatamanchuk <nathaliaatamanchuk5@gmail.com>
2023-09-26 14:09:15 -0300

    map function

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,15 +21,8 @@ function convertPokemonToLi(pokemon) {
 }
 
 const pokemonList = document.getElementById('pokemonList')
-pokeApi.getPokemons().then((pokemons) => {
-    const listItems = []
 
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        listItems.push(convertPokemonToLi(pokemon))
-    }
-
-    console.log(listItems)
-
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
 })
 
